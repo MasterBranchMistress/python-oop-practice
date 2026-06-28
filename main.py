60856fd6378af8e765269e92903479daf8fb0c84
masterbranchmistress <masterbranchmistress@gmail.com>
2026-06-28 10:13:14 -0500

    static and class methods

--- main.py
+++ main.py
@@ -11,7 +11,14 @@ class PlayerCharacter:
     def run(self):
         print(f"{self.name} is running at level {self.level}.")
     
+    @classmethod
+    def add_things(cls, num1, num2):
+        return cls("jimmie", num1 + num2)  # Using class method to create an instance
+    
 player1 = PlayerCharacter("Alice", 5)
 player1.display_info()
 player1.run()
-print(player1.name)
+player2addingstuff = PlayerCharacter.add_things(3, 4)  # 
+# Using the class method without creating an instance
+print(f"Sum of numbers using class method: {player2addingstuff}")
+print(PlayerCharacter.add_things(6, 4))  # Using the class method without creating an instance
