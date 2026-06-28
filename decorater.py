2ea856dc9f2d1f8e145628a2dc75c5408b1cdd8d
masterbranchmistress <masterbranchmistress@gmail.com>
2026-06-28 12:09:41 -0500

    error handling

--- decorater.py
+++ decorater.py
@@ -1,3 +1,11 @@
+#Decorators are a very powerful and useful tool in 
+# Python since it allows programmers to modify 
+# the behavior of function or class. 
+# Decorators allow us to wrap another function in 
+# order to extend the behavior of the 
+# wrapped function, without permanently 
+# modifying it.
+
 def myDecorator(func):
     def wrapper(*args, **kwargs):
         print("Something is happening before the function is called.")
