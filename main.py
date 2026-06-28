420fb04d36339472174ebe16668d0512d04882c0
masterbranchmistress <masterbranchmistress@gmail.com>
2026-06-28 09:46:25 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# OOP
+class PlayerCharacter:
+    def __init__(self, name, level):
+        self.name = name
+        self.level = level
+
+    def display_info(self):
+        print(f"Player Name: {self.name}, Level: {self.level}")
+
+    def run(self):
+        print(f"{self.name} is running at level {self.level}.")
+    
+player1 = PlayerCharacter("Alice", 5)
+player1.display_info()
+player1.run()
+print(player1.name)
